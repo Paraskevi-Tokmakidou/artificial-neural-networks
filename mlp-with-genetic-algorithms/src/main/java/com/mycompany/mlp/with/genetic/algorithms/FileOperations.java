11c56265734d2c20b05e2d4c4244c6db07450189
Paraskevi Tokmakidou <paraskevitokmakidou@gmail.com>
2025-04-02 23:54:20 +0300

    Use try-with-resources instead of try in order to close automatically the used sources for files.

--- mlp-with-genetic-algorithms/src/main/java/com/mycompany/mlp/with/genetic/algorithms/FileOperations.java
+++ mlp-with-genetic-algorithms/src/main/java/com/mycompany/mlp/with/genetic/algorithms/FileOperations.java
@@ -5,68 +5,65 @@ package com.mycompany.mlp.with.genetic.algorithms;
  * @author Paraskevi Tokmakidou
  */
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import javax.swing.JFileChooser;
 
 public class FileOperations {
+
     public boolean loadDataset(String fileName) {
-        if (fileName.isEmpty())
+        if (fileName.isEmpty()) {
             return false;
+        }
 
         ArrayList<ArrayList<Double>> patterns = new ArrayList<>();
         boolean isValid = true;
         int dimension = -1;
 
-        try {
-            FileReader file = new FileReader(fileName);
-            try (Scanner in = new Scanner(file)) {
-                int i = 0;
-                // Read the file line-by-line
-                while (in.hasNextLine()) {
-                    if (isValid) {
-                        String line = in.nextLine();
+        try (FileReader file = new FileReader(fileName); Scanner in = new Scanner(file)) {
+            int i = 0;
+            // Read the file line-by-line
+            while (in.hasNextLine()) {
+                if (isValid) {
+                    String line = in.nextLine();
 
-                        if (line.startsWith("//")) // For comments
-                            continue;
+                    if (line.startsWith("//")) // For comments
+                        continue;
 
-                        ArrayList<Double> newPattern = new ArrayList<>();
-                        String[] characteristics = line.split(",");
-                        if (i == 0) {
-                            dimension = characteristics.length;
-                            for (String characteristic : characteristics) {
-                                newPattern.add(Double.valueOf(characteristic));
-                            }
-                            patterns.add(newPattern);
-                        } else if (characteristics.length == dimension) {
-                            for (String characteristic : characteristics) {
-                                newPattern.add(Double.valueOf(characteristic));
-                            }
-                            patterns.add(newPattern);
-                        } else {
-                            isValid = false;
+                    ArrayList<Double> newPattern = new ArrayList<>();
+                    String[] characteristics = line.split(",");
+                    if (i == 0) {
+                        dimension = characteristics.length;
+                        for (String characteristic : characteristics) {
+                            newPattern.add(Double.valueOf(characteristic));
                         }
-                    }
-                }
-
-                if (isValid && fileName.endsWith(".train")) {
-                    Data.setTrainPatterns(patterns);
-                    Data.setDimension(dimension - 1); // The last column is the output class of pattern
-                } else if (isValid && fileName.endsWith(".test")) {
-                    if ((dimension - 1) == Data.getDimension()) {
-                        Data.setTestPatterns(patterns);
+                        patterns.add(newPattern);
+                    } else if (characteristics.length == dimension) {
+                        for (String characteristic : characteristics) {
+                            newPattern.add(Double.valueOf(characteristic));
+                        }
+                        patterns.add(newPattern);
                     } else {
                         isValid = false;
                     }
-
                 }
-
-                return isValid;
             }
-        } catch (FileNotFoundException | NumberFormatException ex) {
-            System.out.println("Cannot read " + fileName + " file");
+
+            if (isValid && fileName.endsWith(".train")) {
+                Data.setTrainPatterns(patterns);
+                Data.setDimension(dimension - 1); // The last column is the output class of pattern
+            } else if (isValid && fileName.endsWith(".test")) {
+                if ((dimension - 1) == Data.getDimension()) {
+                    Data.setTestPatterns(patterns);
+                } else {
+                    isValid = false;
+                }
+            }
+
+            return isValid;
+        } catch (NumberFormatException | IOException ex) {
             return false;
         }
     }
